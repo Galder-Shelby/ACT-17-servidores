7edb7257d736246b5bbab46ccb013c57eac72d23
Jadak Shelby <>
2023-03-29 14:06:36 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php
+      
+      $nombre = "Meza Ceballos Galder Gabriel";
+      echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//ES" ,
+      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
+      echo '<html xmlns="http://www.w3.org/1999/xhtml">';
+      echo '<head>';
+      echo '<title>Actividad 17 Servidores</title>';
+      echo '</head>';
+      echo '<body>';
+      echo '<p>';
+
+      echo "Buenos días, tardes o noches 😎👋 $nombre!<br/>";
+ echo '<p>';
+    echo 'CALLE: Avenida Juárez.<p>';
+      echo 'Mi número de casa #624<p>';
+      echo 'Mi colonia es:guayabitos';
+      echo 'Mi municipio es: SALTILLO<p>';
+      echo 'Mi numero de teléfono es: +52 844 234 6789<p>';
+      echo '</p>';
+      echo 'Hoy es 29 del mes de marzo del año 2023.<p>'; //comentario
+      echo '<p>';
+      echo '</p>';
+      echo '</body>';
+      echo '</html>';
+      ?> 
+    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+  </body>
+</html>
